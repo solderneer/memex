641158e7d97734c4471d1390dff80730b5cb2f13
Sudharshan <sudhar393@gmail.com>
2023-06-28 09:58:50 +0530

    feat: adding semantic search and indexing endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Flask imports
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 # Local modules
 from index import Index
@@ -8,19 +9,23 @@ index = Index("./embeddings", ["./notes", "./letters", "./_private"])
 index.refresh()
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/index")
-def index():
-    pass
+@app.route("/refresh")
+def refresh():
+    total, skipped, error = index.refresh()
+    return {
+        "total": total,
+        "skipped": skipped,
+        "error": error
+    }
 
 @app.route("/search")
 def search():
-    return "hi"
+    query = request.args.get('query', '')
+    limit = int(request.args.get('limit', '5'))
+    return index.search(query, limit)
 
 @app.route("/answer")
 def answer():
     return "hi"
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
